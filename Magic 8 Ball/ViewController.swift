bf468c1a1939c266143edd0dae3b379e70461a7f
timotheekim10 <timotheekim10@gmail.com>
2023-03-07 11:19:59 +0900

    Use Code to Change the 8 Ball Image

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = #imageLiteral(resourceName: "ball3")
     }
     
 }
